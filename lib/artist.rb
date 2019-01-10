824f42b51369f5566a3476eb45d8a1854c98ac97
github username <github email address>
2019-01-09 19:47:45 -0500

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,9 @@
+class Artist
+
+  attr_accessor :name
+
+  def artist=(artist)
+    artist = Artist.new()
+  end
+
+end
---
+++ lib/author.rb
@@ -0,0 +1,9 @@
+class Author
+
+  attr_accessor :name
+
+  def author=(author)
+    @author = Author.new()
+  end
+
+end
